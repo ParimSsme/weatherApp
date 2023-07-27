fa758dae5f4fd93498d0214ac2b228dee8f679c4
ParimSsme <parima.ssme@gmail.com>
2023-07-27 12:33:26 +0430

    refactor codes

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -104,163 +104,168 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-    if (!isDataLoaded) {
-      return const LoadingWidget();
-    } else {
-      return Scaffold(
-        resizeToAvoidBottomInset: false,
-        backgroundColor: kOverLayColor,
-        body: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Row(
-                children: [
-                  Expanded(
-                    flex: 5,
-                    child: Padding(
-                      padding: const EdgeInsets.all(10),
-                      child: TextField(
-                        decoration: textFieldInputDecoration,
-                        onSubmitted: (String typedName) {
-                          setState(() {
-                            isDataLoaded = false;
-                            updateUI(cityName: typedName);
-                          });
-                        },
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    flex: 5,
-                    child: Padding(
-                      padding: const EdgeInsets.only(right: 5),
-                      child: ElevatedButton(
-                        onPressed: () {
-                          setState(() {
-                            isDataLoaded = false;
-                            getPermission();
-                          });
-                        },
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.white10,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                        ),
-                        child: SizedBox(
-                          height: 67,
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text(
-                                'My Location',
-                                style: kTextFieldTextStyle,
-                              ),
-                              const SizedBox(
-                                width: 4,
-                              ),
-                              const Icon(Icons.my_location),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              isErrorOccured
-                  ? ErrorMessage(title: title!, message: message!)
-                  : Expanded(
-                      child: Column(
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            const Icon(
-                              Icons.location_city,
-                            ),
-                            Text(
-                              weatherModel!.location!,
-                              style: kLocationTextStyle,
-                            ),
-                          ],
-                        ),
-                        SvgPicture.asset(
-                          weatherModel!.icon!,
-                          color: kLightColor,
-                          height: 180,
-                        ),
-                        const SizedBox(
-                          height: 40,
-                        ),
-                        Text(
-                          "${weatherModel != null ? weatherModel!.temperature! : 0}\u00B0",
-                          style: kTempTextStyle,
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(left: 12),
-                          child: Text(
-                            weatherModel!.description!,
-                            style: kLocationTextStyle,
-                          ),
-                        ),
-                      ],
-                    )),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Card(
-                  color: kOverLayColor,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                  child: SizedBox(
-                    height: 90,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Expanded(
-                          child: DetailsWidgets(
-                              value:
-                                  '${weatherModel != null ? weatherModel!.feelsLike!.round() : 0}°',
-                              title: 'FEELS LIKE'),
-                        ),
-                        const Padding(
-                          padding: EdgeInsets.symmetric(vertical: 15),
-                          child: VerticalDivider(
-                            thickness: 1,
-                          ),
-                        ),
-                        Expanded(
-                          child: DetailsWidgets(
-                              value:
-                                  '${weatherModel != null ? weatherModel!.humidity! : 0}%',
-                              title: 'HUMIDITY'),
-                        ),
-                        const Padding(
-                          padding: EdgeInsets.symmetric(vertical: 15),
-                          child: VerticalDivider(
-                            thickness: 1,
-                          ),
-                        ),
-                        Expanded(
-                          child: DetailsWidgets(
-                              value:
-                                  '${weatherModel != null ? weatherModel!.wind!.round() : 0}',
-                              title: 'WIND'),
-                        ),
-                      ],
+    return !isDataLoaded  ? const LoadingWidget() : const LoadedData();
+  }
+}
+
+class LoadedData extends StatelessWidget {
+  const LoadedData({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: kOverLayColor,
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Row(
+              children: [
+                Expanded(
+                  flex: 5,
+                  child: Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: TextField(
+                      decoration: textFieldInputDecoration,
+                      onSubmitted: (String typedName) {
+                        // setState(() {
+                        //   isDataLoaded = false;
+                        //   updateUI(cityName: typedName);
+                        // });
+                      },
                     ),
                   ),
                 ),
+                Expanded(
+                  flex: 5,
+                  child: Padding(
+                    padding: const EdgeInsets.only(right: 5),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        // setState(() {
+                        //   isDataLoaded = false;
+                        //   getPermission();
+                        // });
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.white10,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                      child: SizedBox(
+                        height: 67,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              'My Location',
+                              style: kTextFieldTextStyle,
+                            ),
+                            const SizedBox(
+                              width: 4,
+                            ),
+                            const Icon(Icons.my_location),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            // isErrorOccured
+            //     ? ErrorMessage(title: title!, message: message!)
+            //     : Expanded(
+            //     child: Column(
+            //       children: [
+            //         Row(
+            //           mainAxisAlignment: MainAxisAlignment.center,
+            //           children: [
+            //             const Icon(
+            //               Icons.location_city,
+            //             ),
+            //             Text(
+            //               weatherModel!.location!,
+            //               style: kLocationTextStyle,
+            //             ),
+            //           ],
+            //         ),
+            //         SvgPicture.asset(
+            //           weatherModel!.icon!,
+            //           color: kLightColor,
+            //           height: 180,
+            //         ),
+            //         const SizedBox(
+            //           height: 40,
+            //         ),
+            //         Text(
+            //           "${weatherModel != null ? weatherModel!.temperature! : 0}\u00B0",
+            //           style: kTempTextStyle,
+            //         ),
+            //         Padding(
+            //           padding: const EdgeInsets.only(left: 12),
+            //           child: Text(
+            //             weatherModel!.description!,
+            //             style: kLocationTextStyle,
+            //           ),
+            //         ),
+            //       ],
+            //     )),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Card(
+                color: kOverLayColor,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: SizedBox(
+                  height: 90,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Expanded(
+                        child: DetailsWidgets(
+                            value:
+                            '${weatherModel != null ? weatherModel!.feelsLike!.round() : 0}°',
+                            title: 'FEELS LIKE'),
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.symmetric(vertical: 15),
+                        child: VerticalDivider(
+                          thickness: 1,
+                        ),
+                      ),
+                      Expanded(
+                        child: DetailsWidgets(
+                            value:
+                            '${weatherModel != null ? weatherModel!.humidity! : 0}%',
+                            title: 'HUMIDITY'),
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.symmetric(vertical: 15),
+                        child: VerticalDivider(
+                          thickness: 1,
+                        ),
+                      ),
+                      Expanded(
+                        child: DetailsWidgets(
+                            value:
+                            '${weatherModel != null ? weatherModel!.wind!.round() : 0}',
+                            title: 'WIND'),
+                      ),
+                    ],
+                  ),
+                ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
-      );
-    }
+      ),
+    );
   }
 }
